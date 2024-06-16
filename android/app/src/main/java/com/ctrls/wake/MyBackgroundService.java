d294d94afd1719749218f476b3f0a666ff00cec4
dev-trams <kppcgold@gmail.com>
2024-06-17 02:51:02 +0900

    blue_example

---
+++ android/app/src/main/java/com/ctrls/wake/MyBackgroundService.java
@@ -0,0 +1,12 @@
+import java.security.Provider;
+import java.security.Provider.Service;
+import java.util.List;
+import java.util.Map;
+
+public class MyBackgroundService extends Service {
+
+    public static final String TAG = "MyBackgroundService";
+
+
+
+}
---
+++ android/app/src/main/kotlin/com/example/blue_plus_example/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.blue_plus_example
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
